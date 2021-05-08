ea48eb7fc00b2344501290052c416d3a2d04413f
Pivansm <pivansm@outlook.com>
2021-05-08 18:06:28 +0300

    "Project hello-maven v1.0"

--- src/main/java/maven/ru/sberbank/restert/HelloWorld.java
+++ src/main/java/maven/ru/sberbank/restert/HelloWorld.java
@@ -1,11 +1,12 @@
-package ru.sberbank.restart;
+package maven.ru.sberbank.restert;
 
+import org.apache.commons.lang3.*;
 /**
  * Текст класса HelloWorld
  * @author Ivan
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!" );
+        System.out.println(StringUtils.upperCase("hello world!" ));
     }
 }
